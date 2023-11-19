e2e46f1f700fb6dbc3a237b0729d0fa83c2a2db2
Jona <zS19023589@estudiantes.uv.mx>
2023-11-19 17:42:49 -0600

    view RegisterDriver y model DriverClient

---
+++ HDIClient/Models/DriverClient.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AseguradoraApp.Models
+{
+    public class DriverClient
+    {
+        [Required(ErrorMessage ="El campo es obligatorio")]
+        [Display(Name = "")]
+        public string DriverName { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [Display(Name = "")]
+        public string DriverLastname { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [Display(Name = "")]
+        public string TelephoneNumber { get; set; }
+
+        [Required(ErrorMessage = "El campo es obligatorio")]
+        [Display(Name = "")]
+        public string License { get; set; }
+    }
+}
