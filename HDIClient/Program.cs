51ebe2cb430d2f5d05ac547d2d31411517979e8b
Cristopher RoSa <32348461+CristopherRoSa@users.noreply.github.com>
2024-01-02 01:08:43 -0600

    Commit para actualizar rama.

--- HDIClient/Program.cs
+++ HDIClient/Program.cs
@@ -1,6 +1,7 @@
 using HDIClient.Service;
 using HDIClient.Service.Interface;
 
+
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
