d1e3048da5499e62a759496f217794b118a8f813
EDUART USSIEL DIRCIO CAYON <zS19030173@estudiantes.uv.mx>
2024-01-04 14:23:08 -0600

    se agrego clase para pruebas de el servicio de report

---
+++ TestProject/ReportTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HDIClient.Service;
+using HDIClient.Service.Interface;
+
+namespace TestProject
+{
+    public class ReportTest
+    {
+        private readonly IPolicyService _service;
+        private readonly IReportService _reportService;
+
+        public ReportTest(IPolicyService service, IReportService reportService)
+        {
+            _service = service;
+            _reportService = reportService;
+
+        }
+
+        [Fact]
+        public void TestGetReportById()
+        {
+
+        }
+    }
+}
--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -4,16 +4,8 @@ using HDIClient.Service.Interface;
 namespace TestProject
 {
     public class UnitTest1
-    {
-        private readonly IPolicyService _service;
-        private readonly IReportService _reportService;
-
-        public UnitTest1(IPolicyService service, IReportService reportService)
-        {
-            _service = service;
-            _reportService = reportService;
-
-        }
+    { 
+        
         [Fact]
         public void Test1()
         {
